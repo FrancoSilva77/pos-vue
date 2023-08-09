dbedeeacec026de20eb1b91ed3e3cef4602d479a
Franco Silva <franco_tigres18@hotmail.com>
2023-08-09 14:51:38 -0600

    Eliminar Productos add

--- src/stores/products.js
+++ src/stores/products.js
@@ -1,6 +1,6 @@
 import { computed } from 'vue';
 import { defineStore } from 'pinia';
-import { useFirestore, useCollection } from 'vuefire';
+import { useFirestore, useCollection, useFirebaseStorage } from 'vuefire';
 import {
   collection,
   addDoc,
@@ -9,10 +9,15 @@ import {
   limit,
   orderBy,
   updateDoc,
+  doc,
+  getDoc,
+  deleteDoc,
 } from 'firebase/firestore';
+import { ref as storageRef, deleteObject } from 'firebase/storage';
 
 export const useProductsStore = defineStore('products', () => {
   const db = useFirestore();
+  const storage = useFirebaseStorage();
 
   const categories = [
     { id: 1, name: 'Sudaderas' },
@@ -38,6 +43,17 @@ export const useProductsStore = defineStore('products', () => {
     }
   }
 
+  async function deleteProduct(id) {
+    if (confirm('¿Eliminar Producto?')) {
+      const docRef = doc(db, 'products', id);
+      const docSnap = await getDoc(docRef);
+      const { image } = docSnap.data();
+      const imageRef = storageRef(storage, image);
+
+      await Promise.all([deleteDoc(docRef), deleteObject(imageRef)]);
+    }
+  }
+
   const categoryOptions = computed(() => {
     const options = [
       { label: '--Seleccione--', value: '', attrs: { disabled: true } },
@@ -54,6 +70,7 @@ export const useProductsStore = defineStore('products', () => {
   return {
     createProduct,
     updateProduct,
+    deleteProduct,
     productsCollection,
     categoryOptions,
     noResults,
